d9f4523a4a3e34aae588b5b4c8dd8265c791b3ff
manitu85 <manitu.psy@gmail.com>
2021-03-22 21:28:54 +0100

    add client side data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import useLatestData from 'hooks/useLatestData';
 
-const HomePage = () => (
-  <>
-    <h1>Hello Pizza</h1>
-  </>
-);
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
-export default HomePage;
+export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
+
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
+  );
+}
