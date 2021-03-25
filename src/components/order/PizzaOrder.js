bb0fb6341581e5668ab3830a94fca71ae44fb0a8
manitu85 <manitu.psy@gmail.com>
2021-03-25 14:43:28 +0100

    #

--- src/components/order/PizzaOrder.js
+++ src/components/order/PizzaOrder.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Img from 'gatsby-image';
 import { formatMoney, calculatePizzaPrice } from 'utils';
 
-import MenuItemStyles from './menu-item.styles';
+import MenuItemStyles from './menuItem.styles';
 
 export default function PizzaOrder({ order, pizzas, removeFromOrder }) {
   return (
